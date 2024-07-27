3f2c63801959a07fb8ca21d87b9388b022984a80
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-28 00:23:38 +0530

    .

--- app/src/main/java/com/example/adminyummycart/SignUpActivity.kt
+++ app/src/main/java/com/example/adminyummycart/SignUpActivity.kt
@@ -1,16 +1,25 @@
 package com.example.adminyummycart
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adminyummycart.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {4
+    private val binding:ActivitySignUpBinding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+        val locationList = arrayOf("Allahabad","Varanasi","Gorakhpur","Basti")
+        val adapter =ArrayAdapter(this,android.R.layout.simple_list_item_1,locationList)
+        val autoCompleteTextView = binding.lisoflocation
+        autoCompleteTextView.setAdapter(adapter)
 
     }
 }
