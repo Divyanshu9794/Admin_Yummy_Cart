ac54a73bc073dd22a4bafcdff4a3cc0807403707
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-27 13:17:36 +0530

    .

--- app/src/main/java/com/example/adminyummycart/SplashScreenActivity.kt
+++ app/src/main/java/com/example/adminyummycart/SplashScreenActivity.kt
@@ -1,6 +1,9 @@
 package com.example.adminyummycart
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+
     }
+}
+
+private fun Handler.postDelayed(function: () -> Unit) {
+
 }
