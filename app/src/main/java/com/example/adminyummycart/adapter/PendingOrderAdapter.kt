7ed34e499fe55775f1b7144eceba2dbc59b650c2
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-31 00:43:01 +0530

    ,

---
+++ app/src/main/java/com/example/adminyummycart/adapter/PendingOrderAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.adminyummycart.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.adminyummycart.PendingOrderActivity
+
+class PendingOrderAdapter(): RecyclerView.Adapter<PendingOrderAdapter.PendingOrderViewHolder>() {
+    class PendingOrderViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PendingOrderViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PendingOrderViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
