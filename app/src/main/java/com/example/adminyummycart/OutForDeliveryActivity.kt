98fcee33c87a4faea143a8dbec80434aa80451b0
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-30 20:47:31 +0530

    .

---
+++ app/src/main/java/com/example/adminyummycart/OutForDeliveryActivity.kt
@@ -0,0 +1,16 @@
+package com.example.adminyummycart
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OutForDeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_out_for_delivery)
+
+    }
+}
