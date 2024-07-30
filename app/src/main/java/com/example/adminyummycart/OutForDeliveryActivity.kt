165ff7761f3446daad9422514d87f825f093a997
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-07-30 21:11:45 +0530

    ,

--- app/src/main/java/com/example/adminyummycart/OutForDeliveryActivity.kt
+++ app/src/main/java/com/example/adminyummycart/OutForDeliveryActivity.kt
@@ -5,12 +5,32 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.adminyummycart.adapter.DeliveryAdapter
+import com.example.adminyummycart.databinding.ActivityOutForDeliveryBinding
 
 class OutForDeliveryActivity : AppCompatActivity() {
+    private val binding : ActivityOutForDeliveryBinding by lazy{
+        ActivityOutForDeliveryBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_out_for_delivery)
+        setContentView(binding.root)
+
+        val customerName = arrayListOf(
+            "Divyanshu Singh",
+            "Amar Singh ",
+            "Dhruv Dwivedi"
+        )
+        val moneyStatus = arrayListOf(
+            "received",
+            "notReceived",
+            "pending"
+        )
+        val adapter = DeliveryAdapter(customerName,moneyStatus)
+        binding.DeliveryRecyclerView.adapter = adapter
+        binding.DeliveryRecyclerView.layoutManager= LinearLayoutManager(this)
 
     }
 }
