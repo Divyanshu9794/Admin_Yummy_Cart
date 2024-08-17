ff815036898cd76ca012eb688cbdd287d6d229be
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-17 18:08:57 +0530

    .

--- app/src/main/java/com/example/adminyummycart/MainActivity.kt
+++ app/src/main/java/com/example/adminyummycart/MainActivity.kt
@@ -7,15 +7,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminyummycart.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     private val binding:ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var database:FirebaseDatabase
+    private lateinit var auth :FirebaseAuth
+
+    private lateinit var completedOrderReference : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+
         binding.addmenu.setOnClickListener {
             val intent = Intent(this,AddItemActivity::class.java)
             startActivity(intent)
@@ -42,6 +54,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this,PendingOrderActivity::class.java)
             startActivity(intent)
         }
+
+        pendingOrders()
+
+    }
+
+    private fun pendingOrders() {
+        database = FirebaseDatabase.getInstance()
+        var pendingOrderReference = database.reference.child("OrderDetails")
+        var pendingOrderItemCount =0
+        pendingOrderReference.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                pendingOrderItemCount=snapshot.childrenCount.toInt()
+                binding.pendingOrders.text = pendingOrderItemCount.toString()
+
+
+                //9:23:09 tak hua h baaki karna h aur comment hata dena h
+                //9:23:09 tak hua h baaki karna h aur comment hata dena h
+                //9:23:09 tak hua h baaki karna h aur comment hata dena h
+                //9:23:09 tak hua h baaki karna h aur comment hata dena h
+                //9:23:09 tak hua h baaki karna h aur comment hata dena h
+                
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
 
     }
 }
