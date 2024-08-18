fa90aab025babd1ca023d9aaae222cd9f154bcc5
Divyanshu9794 <sdivyanshu352@gmail.com>
2024-08-18 12:53:52 +0530

    .

--- app/src/main/java/com/example/adminyummycart/AdminProfileActivity.kt
+++ app/src/main/java/com/example/adminyummycart/AdminProfileActivity.kt
@@ -7,8 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.adminyummycart.databinding.ActivityAdminProfileBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AdminProfileActivity : AppCompatActivity() {
     private val binding:ActivityAdminProfileBinding by lazy {
@@ -29,7 +32,8 @@ class AdminProfileActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
         adminReference = database.reference.child("user")
-        
+
+
         binding.backbutton.setOnClickListener {
             finish()
         }
@@ -39,6 +43,8 @@ class AdminProfileActivity : AppCompatActivity() {
         binding.emailid.isEnabled=false
         binding.phonenumber.isEnabled=false
         binding.password.isEnabled=false
+        binding.saveInfoButton.isEnabled = false
+
 
         var isEnable = false
         binding.editButton.setOnClickListener{
@@ -55,6 +61,46 @@ class AdminProfileActivity : AppCompatActivity() {
             }
 
         }
+
+        retrieveUserData()
+    }
+
+    private fun retrieveUserData() {
+
+        adminReference.addListenerForSingleValueEvent(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    var ownerName = snapshot.child("name").getValue()
+                    var email = snapshot.child("email").getValue()
+                    var password = snapshot.child("password").getValue()
+                    var address = snapshot.child("address").getValue()
+                    var phone = snapshot.child("phone").getValue()
+
+                    setDataToTextView(ownerName,email,password,address,phone)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun setDataToTextView(
+        ownerName: Any?,
+        email: Any?,
+        password: Any?,
+        address: Any?,
+        phone: Any?
+    ) {
+
+        binding.name.setText(ownerName.toString())
+        binding.emailid.setText(email.toString())
+        binding.password.setText(password.toString())
+        binding.address.setText(address.toString())
+        binding.phonenumber.setText(phone.toString())
+
     }
 
 }
